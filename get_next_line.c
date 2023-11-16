7ed1f6b45252461a81d79bf62770c88ba130c330
SaltyChickenLeg <merijnjong1@gmail.com>
2023-11-16 18:07:35 +0100

    started learning about read function and how to allocate correctly while using read

--- get_next_line.c
+++ get_next_line.c
@@ -6,18 +6,56 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:06:50 by mjong             #+#    #+#             */
-/*   Updated: 2023/11/14 13:14:14 by mjong            ###   ########.fr       */
+/*   Updated: 2023/11/16 17:57:09 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*get_next_line(char *s)
+ssize_t	count_first_line(int fd)
 {
-	
+	char	*result;
+	ssize_t	count;
+
+	result = write_first_line(fd);
+	count = 0;
+	while (result[count] != '\0')
+	{
+		count++;
+	}
+	free(result);								// free 2
+	return (count);
+}
+
+char	*write_first_line(int fd)
+{
+	char		*buf;
+	char		*line;
+	int			i;
+
+	buf = (char *)malloc(BUFFER_SIZE); 			// allocate 1
+	line = (char *)malloc(BUFFER_SIZE); 		// allocate 2
+	i = 0;
+	if (buf == NULL)
+		return (0);
+	read(fd, buf, BUFFER_SIZE);					// read function writes in buf
+	while (buf[i] != '\n')
+	{
+		line[i] = buf[i];
+		i++;
+	}
+	free(buf);									// free 1
+	return (line);
 }
 
 int	main(void)
 {
-	
+	int	fd;
+	ssize_t result;
+
+	fd = open("text.txt", O_RDONLY);
+	result = count_first_line(fd);
+	printf("%zu", result);
+	close(fd);
+	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,26 @@
 /*   By: mjong <mjong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 13:07:12 by mjong             #+#    #+#             */
-/*   Updated: 2023/11/14 13:13:47 by mjong            ###   ########.fr       */
+/*   Updated: 2023/11/16 17:51:18 by mjong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
 
-char	*get_next_line(char *s);
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 300
+#endif
+// ssize_t	read_result(int fd);
+// char	*write_from_fd(int fd);
+
+ssize_t	count_first_line(int fd);
+char 	*write_first_line(int fd);
 
 #endif
